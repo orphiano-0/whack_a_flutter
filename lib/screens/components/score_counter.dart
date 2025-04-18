8498972b4151407771e8b76cd785c6609cc9c866
Gabriel Orphiano <gab.orphiano@gmail.com>
2025-04-18 19:53:09 +0800

    added score widget

--- lib/screens/components/score_counter.dart
+++ lib/screens/components/score_counter.dart
@@ -1,10 +1,94 @@
 import 'package:flutter/material.dart';
 
-class ScoreCounter extends StatelessWidget {
-  const ScoreCounter({super.key});
+class ScoreCounter extends StatefulWidget {
+  final int score;
+
+  const ScoreCounter({super.key, required this.score});
+
+  @override
+  State<ScoreCounter> createState() => _ScoreCounterState();
+}
+
+class _ScoreCounterState extends State<ScoreCounter>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  int _previousScore = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _previousScore = widget.score;
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.2,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutBack));
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      }
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant ScoreCounter oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.score != oldWidget.score) {
+      _controller.forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScaleTransition(
+      scale: _scaleAnimation,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          color: Colors.orange.shade100,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.orange.shade300,
+              offset: const Offset(4, 4),
+              blurRadius: 8,
+            ),
+            const BoxShadow(
+              color: Colors.white,
+              offset: Offset(-4, -4),
+              blurRadius: 8,
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Icon(Icons.star, color: Colors.deepOrange),
+            const SizedBox(width: 8),
+            Text(
+              'Score: ${widget.score}',
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepOrange,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
